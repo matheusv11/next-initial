73aad4a4666e953a2d4cc7caf84e30d9e0d39610
matheusv11 <mathsales360@gmail.com>
2021-09-05 18:36:20 -0300

    Paths estaticos

--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -1,12 +1,52 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router'; //CLIENT-SIDE
+// export async function getServerSideProps(context) { //SERVER-SIDE RODA NO BACKENDS
+//     const id = context.query.id;
 
-function Produtos () {
-    const router = useRouter();
-    const id = router.query.id;
+//     return {
+//         props: {
+//            id: id
+//         }
+//     }
+// }
+
+export async function getStaticPaths () {
+    return {
+        paths: [
+            {
+                params: {
+                    id: '1'
+                }
+            },
+            {
+                params: {
+                    id: '2'
+                }
+            }
+        ],
+        fallback: 'blocking'  //FALSE //TRUE //BLOCKING
+    }
+}
+
+export async function getStaticProps (context) {
+    await delay(5000);
+    const id = context.params.id;
+
+    return {
+        props:{
+            id: id
+        }
+    }
+}
+
+function Produtos (props) {
+    // const router = useRouter();
+    // const id = router.query.id;
 
     return (
-        <div>ID do produto: {id}</div>
+        <div>ID do produto: {props.id}</div>
     )
 }
 
 export default Produtos;
+
+const delay = ms => new Promise(resolve=> setTimeout(resolve, ms));
